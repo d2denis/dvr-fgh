eb2cab2107fc1086be34ac8a2b95f0f40ea8a643
d2denis <zhjpan@gmail.com>
2018-05-08 22:31:59 +0800

    ready for multi mode fitting
    using native group to construct parameters:dipole,polarizability,Q,and gamma

--- src/apps/deomfit.cc
+++ src/apps/deomfit.cc
@@ -48,6 +48,7 @@ using namespace json11;
     const double staticErr = 2e-7;//
     const int    nk = 32;//
     const string sch_hei = "s";//
+  const int  num_para = 4;
 
 mat matprod(cube a, vec b){
     mat result = zeros<mat>(size(a.slice(0)));
@@ -66,7 +67,43 @@ cube cubprod(cube a, vec b){
     }
     return result;
 }
+cube cubprod2(cube a, vec b){
+    cube result = zeros<cube>(size(a));
+    uword nmds = b.n_elem;
 
+    for(uword i=0;i<nmds;++i){
+        result.slice(i) = a.slice(i)*a.slice(i)*b(i);
+    }
+    return result;
+}
+
+int json2para(const uword nmds,double* d1,double* d2,double* p1,double* p2,double* q1,double* q2,double* gamd){
+    ifstream jsonFile("morse.json");
+    stringstream strStream;
+    strStream << jsonFile.rdbuf();
+    string jsonStr = strStream.str();
+    string err;
+
+    const Json json = Json::parse(jsonStr,err);
+    if (!err.empty()) {
+        printf ("Error in parsing input file: %s\n", err.c_str());
+        return 0;
+    }
+
+    Json mds = json["modes"];
+
+    for (uword nmode=0;nmode<nmds;nmode++){
+      d1[nmode] = mds[nmode]["d1"].number_value();
+      d2[nmode] = mds[nmode]["d2"].number_value();
+      p1[nmode] = mds[nmode]["p1"].number_value();
+      p2[nmode] = mds[nmode]["p2"].number_value();
+      q1[nmode] = mds[nmode]["q1"].number_value();
+      q2[nmode] = mds[nmode]["q2"].number_value();
+//      lamd[nmode] = mds[nmode]["jdru"][0]["lamd"].number_value()*cm2unit;
+      gamd[nmode] = mds[nmode]["jdru"][0]["gamd"].number_value()*cm2unit;
+    }
+    return 1;
+}
 int setjson(Json& mds,const uword nmds,vec& d1,vec& d2,vec& q1,vec& q2,vec& lamd,vec& gamd){
     ifstream jsonFile("morse.json");
     stringstream strStream;
@@ -158,7 +195,7 @@ void resp1 (vec& fw_im, const double w_max, const int mynt, const double dt,
 
 vec fit1d(const cx_mat& hm,const cube& dip,const cube& pol,vec d1,vec d2,vec q1,vec q2,vec lamd,vec gamd) {
 
-    cx_cube qm =deom_c1*(cubprod(dip,q1)+cubprod(pol,q2));
+    cx_cube qm =deom_c1*(cubprod(dip,q1)+cubprod2(dip,q2));
     syst s(hm,qm);
     bath b(npsd,pade,temp,lamd,gamd);
     cx_vec index = b.expn_gam;
@@ -195,13 +232,16 @@ class BALProblem {
 
     observations_ = new double[num_ob];
 
-    FscanfOrDie(fptr, "%d", &num_parameters_);
+/*    FscanfOrDie(fptr, "%d", &num_parameters_);
     parameters_ = new double[num_parameters_];
     for (int i = 0; i < num_parameters_; ++i) {
       FscanfOrDie(fptr, "%lf", parameters_ + i);
     }
-
-
+*/
+    num_parameters_ = nmds*num_para;
+    parameters_ = new double[num_parameters_];
+    double nimei[3];
+    json2para(nmds,parameters_,nimei,nimei,nimei,parameters_+nmds,parameters_+2*nmds,parameters_+3*nmds);
     for (int i = 0; i < num_ob; ++i) {
         FscanfOrDie(fptr, "%lf", observations_ + i );
     }
@@ -223,13 +263,20 @@ class BALProblem {
   double* observations_;
   double* parameters_;
 };
-
+//array to vector
+vec a2v(const double* x){
+    vec v(nmds);
+    for(uword i=0;i<nmds;i++){
+        v(i) = *(x+i);
+    }
+    return v;
+}
 struct Resp1Residual {
   Resp1Residual( double* y)
       :  y_(y) {}
 
 //  template <typename T> bool operator()(const T* const m,
-   bool operator()(const double* const m,  double* residual) const {
+   bool operator()(const double* const d,const double* const q,const double* const qq,const double* const g,  double* residual) const {
   
   Json mds;
   vec  d1(nmds);
@@ -239,15 +286,20 @@ struct Resp1Residual {
   vec  lamd(nmds);
   vec  gamd(nmds);
   if(setjson(mds,nmds,d1,d2,q1,q2,lamd,gamd)){
+//parameter block
+     d1=a2v(d);
+     q1=a2v(q);
+     q2=a2v(qq);
+//     lamd(0)=m[1]*cm2unit; // fixed to avoid over fitted, the factor is q*lamd.
+     gamd=a2v(g);
+//system block
      cx_mat hm;
      cube dip, pol;
      readinp(mds,hm,dip,pol,nmds);
 
      vec fw_im(num_ob);
-     d1(0)=m[0];
-     q1(0)=m[1];
      vec nimei = fit1d(hm,dip,pol,d1,d2,q1,q2, lamd, gamd);
-//     nimei.print("residual1:");
+  nimei.save("fitted.dat",raw_ascii);
      for(int i=0;i<num_ob;i++){
          residual[i] = y_[i]-nimei(i);
      }
@@ -262,49 +314,42 @@ struct Resp1Residual {
 
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
-
+/*
   if (argc != 2) {
     std::cerr << "usage: mycf <bal_problem>\n";
     return 1;
   }
-  
+  */
 
   BALProblem bal_problem;
-  if (!bal_problem.LoadFile(argv[1])) {
+  if (!bal_problem.LoadFile("resp1st_im.w")) {
     std::cerr << "ERROR: unable to open file "<< argv[1]  << "\n";
-    return 1;
-  }
-  const int  num_para = 2;
-  if(bal_problem.num_parameters()!=num_para){
-    std::cerr << "ERROR: number of parameter not match"  << "\n";
     return 1;
   }
   double* datax = bal_problem.mutable_points();
   double* datay = bal_problem.observations();
-  std::cout <<bal_problem.num_parameters()<< " parameters, d: " << datax[0] << " q: " << datax[1] << "\n";
+  if(bal_problem.num_parameters()!=nmds*num_para){
+    std::cerr << "ERROR: number of parameter not match"  << "\n";
+    return 1;
+  }
+  std::cout <<bal_problem.num_parameters()<< " parameters, d: " << datax[0] << " q1: " << datax[1]<< " q2: " << datax[2]<< " gamd: " << datax[3]/cm2unit<< "\n";
   Problem problem;
-  for (int i = 0; i < 1; ++i) {
     problem.AddResidualBlock(
 //        new AutoDiffCostFunction<Resp1Residual, num_ob, 1>(
-        new NumericDiffCostFunction<Resp1Residual,CENTRAL, num_ob, num_para>(
+        new NumericDiffCostFunction<Resp1Residual,CENTRAL, num_ob, nmds,nmds,nmds,nmds>(
             new Resp1Residual(datay)),
         NULL,
-        datax);
-  }
+        datax,datax+1,datax+2,datax+3);
 
   Solver::Options options;
-  options.max_num_iterations = 25;
-  options.linear_solver_type = ceres::DENSE_QR;
+  options.max_num_iterations = 500;
+  options.linear_solver_type = ceres::DENSE_SCHUR;
+//  options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
   options.minimizer_progress_to_stdout = true;
 
   Solver::Summary summary;
   Solve(options, &problem, &summary);
   std::cout << summary.BriefReport() << "\n";
-  std::cout << "Final   d: " << datax[0] << " q: "<< datax[1] <<  "\n";
-  vec nimei(num_ob);
-  for(int i=0;i<num_ob;i++){
-       nimei(i)=datay[i];
-  }
-  nimei.save("fitted.dat",raw_ascii);
+  std::cout << "Final   d: " << datax[0] << " q1: " << datax[1]<< " q2: " << datax[2]<< " gamd: " << datax[3]/cm2unit<< "\n";
   return 0;
 }
